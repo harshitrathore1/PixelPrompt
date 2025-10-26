12926e306f777fe94bd562357a7cebd365ff23e1
harshit <harshitrathore8118@gmail.com>
2025-10-26 10:51:08 +0530

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500 mt-20">
       <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
         <div className="md:max-w-96">
-          <img className="h-9" src={assets.logo} alt="logo" />
+          <img className="h-9" src={assets.PixelPrompt_logo} alt="logo" />
           <p className="mt-6 text-sm">
             We believe in empowering creators and innovators with smart, simple tools. Our mission is to help you transform your ideas into reality, making creativity more accessible and powerful than ever before.
           </p>
